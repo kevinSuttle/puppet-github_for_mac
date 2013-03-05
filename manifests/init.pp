35e545e7e1cb126e2dc7b4c4cb5a81c8d6bef21e
Kevin Suttle <kevin@kevinsuttle.com>
2013-03-05 10:52:20 -0500

    Rename stable.pp to init.pp, repo to puppet-github
    Keeping Puppet's autoloader happy, and keeping app naming conventions.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,7 @@
 # Public: Install Github to /Applications.
 #
-# Examples
-#
-# include github-mac
-class github-mac {
+# include github
+class github {
   package { 'Github':
     provider => 'compressed_app',
     source => 'https://github-central.s3.amazonaws.com/mac/812e9f1a-81f9-11e2-842a-d627744bf6bd.zip',
