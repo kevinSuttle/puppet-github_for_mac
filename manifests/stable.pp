61fc402bd1d9b671a8e0505208a61315e7182c9e
Kevin Suttle <kevin@kevinsuttle.com>
2013-03-04 22:41:24 -0500

    Initial commit

---
+++ manifests/stable.pp
@@ -0,0 +1,11 @@
+# Public: Install Github to /Applications.
+#
+# Examples
+#
+# include githubapp
+class githubapp {
+  package { 'Github':
+    provider => 'compressed_app',
+    source => 'https://github-central.s3.amazonaws.com/mac/812e9f1a-81f9-11e2-842a-d627744bf6bd.zip',
+  }
+}
